130db2e349fbbd63c9e9b5f76e5df6420f3fcf3b
Narek Mailian <narek.mailian@gmail.com>
2021-11-02 16:28:50 +0100

    Update Coded to allow for Data input and decode as data

--- JSONElement/JSONElement+Coded.swift
+++ JSONElement/JSONElement+Coded.swift
@@ -10,18 +10,26 @@ import Foundation
 public extension JSONElement {
     init<T: Encodable>(encodable: T, usingEncoder encoder: JSONEncoder) throws {
         let data = try encoder.encode(encodable)
+        try self.init(data: data)
+    }
+
+    init(data: Data) throws {
         let object = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments)
 
         self = try .init(object)
     }
 
     func decoded<T: Decodable>(usingDecoder decoder: JSONDecoder) throws -> T {
+        return try self.decoded(using: { try decoder.decode(T.self, from: $0) })
+    }
+
+    func decoded<T: Decodable>(using closure: (Data) throws -> T) throws -> T {
         if let decoded = value as? T {
             return decoded
         }
 
         let data = try JSONSerialization.data(withJSONObject: self.value, options: .fragmentsAllowed)
-        let decoded = try decoder.decode(T.self, from: data)
-        return decoded
+        
+        return try closure(data)
     }
 }
